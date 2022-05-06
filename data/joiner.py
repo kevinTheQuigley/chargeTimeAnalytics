7cc6f38c1bcc2801854f9877dea938ee4648f366
kevin <kquigle@tcd.ie>
2022-05-06 21:34:49 +0100

    Creating joiner file between the dowbloaded files

---
+++ data/joiner.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import datetime as dt
+
+weatherHead = pd.read_csv('hly175.csv',skiprows = (0,1,2,3,4,5,6,7,8,9,10,11,12,13))
+weatherHead['date2'] = [dt.datetime.strptime(d,"%d-%b-%Y %H:%M") for d in weatherHead['date'] ]
+weatherHead['date_for_merge'] = weatherHead['date2'].dt.round('H')
+weatherHead = weatherHead[(weatherHead['date2'] >= '2021-04-01') & (weatherHead['date2'] < '2022-04-01')]
+weatherHead =weatherHead.drop(['date_for_merge','date2'], axis=1)
+
+wind = pd.read_csv('wind.csv')
+demand = pd.read_csv('demand.csv')
+generation = pd.read_csv('generation.csv')
+forecastWind = pd.read_csv('forecastWind.csv')
+interconnection = pd.read_csv('interconnection.csv')
+
+wind = wind.drop([' REGION'], axis = 1)
+demand = demand.drop([' REGION'], axis = 1)
+generation = generation.drop([' REGION'], axis = 1)
+forecastWind = forecastWind.drop([' REGION'], axis = 1)
+#interconnection = interconnection.drop(['REGION'], axis = 1)
+
+
+#combined= pd.merge(wind,demand,on='DATE & TIME',how='outer')
+combined= pd.merge(demand ,pd.merge(wind,demand,on='DATE & TIME') ,on = 'DATE & TIME')
+
+
+print(weatherHead.head(20000).to_csv('Combined.csv', index=False))
