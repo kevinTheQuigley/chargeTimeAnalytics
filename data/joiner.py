3779adf8d089cdb99a8d09b5bf7a1c78334246ec
Kevin Quigley <kevinthomassssquigley@gmail.com>
2022-05-09 20:02:53 +0000

    Joining met files

--- data/joiner.py
+++ data/joiner.py
@@ -4,7 +4,7 @@ import datetime as dt
 weatherHead = pd.read_csv('hly175.csv',skiprows = (0,1,2,3,4,5,6,7,8,9,10,11,12,13))
 weatherHead['date2'] = [dt.datetime.strptime(d,"%d-%b-%Y %H:%M") for d in weatherHead['date'] ]
 weatherHead['date_for_merge'] = weatherHead['date2'].dt.round('H')
-weatherHead = weatherHead[(weatherHead['date2'] >= '2021-04-01') & (weatherHead['date2'] < '2022-04-01')]
+weatherHead = weatherHead[(weatherHead['date2'] >= '2019-04-01') & (weatherHead['date2'] < '2022-04-01')]
 dub_weatherHead =weatherHead.drop(['date_for_merge','date'], axis=1)
 dub_weatherHead.columns = ['dub_ind', 'dub_rain', 'dub_ind.1', 'dub_temp', 'dub_ind.2', 'dub_wetb', 'dub_dewpt','dub_vappr', 'dub_rhum', 'dub_msl', 'date2']
 
@@ -12,35 +12,35 @@ dub_weatherHead.columns = ['dub_ind', 'dub_rain', 'dub_ind.1', 'dub_temp', 'dub_
 weatherHead = pd.read_csv('hly2275.csv',skiprows = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21))
 weatherHead['date2'] = [dt.datetime.strptime(d,"%d-%b-%Y %H:%M") for d in weatherHead['date'] ]
 weatherHead['date_for_merge'] = weatherHead['date2'].dt.round('H')
-weatherHead = weatherHead[(weatherHead['date2'] >= '2021-04-01') & (weatherHead['date2'] < '2022-04-01')]
+weatherHead = weatherHead[(weatherHead['date2'] >= '2019-04-01') & (weatherHead['date2'] < '2022-04-01')]
 ker_weatherHead =weatherHead.drop(['date_for_merge','date'], axis=1)
 ker_weatherHead.columns = ['ker_ind', 'ker_rain', 'ker_ind.1', 'ker_temp', 'ker_ind.2', 'ker_wetb', 'ker_dewpt','ker_vappr', 'ker_rhum', 'ker_msl', 'ker_ind.3', 'ker_wdsp', 'ker_ind.4', 'ker_wddir', 'ker_ww', 'ker_w','sun', 'ker_vis', 'ker_clht', 'ker_clamt','date2'] 
 
 weatherHead = pd.read_csv('hly518.csv',skiprows = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21))
 weatherHead['date2'] = [dt.datetime.strptime(d,"%d-%b-%Y %H:%M") for d in weatherHead['date'] ]
 weatherHead['date_for_merge'] = weatherHead['date2'].dt.round('H')
-weatherHead = weatherHead[(weatherHead['date2'] >= '2021-04-01') & (weatherHead['date2'] < '2022-04-01')]
+weatherHead = weatherHead[(weatherHead['date2'] >= '2019-04-01') & (weatherHead['date2'] < '2022-04-01')]
 cla_weatherHead =weatherHead.drop(['date_for_merge','date'], axis=1)
 cla_weatherHead.columns =  ['cla_ind', 'cla_rain', 'cla_ind.1', 'cla_temp', 'cla_ind.2', 'cla_wetb', 'cla_dewpt', 'cla_vappr', 'cla_rhum', 'cla_msl', 'cla_ind.3', 'cla_wdsp', 'cla_ind.4', 'cla_wddir', 'cla_ww', 'cla_w', 'cla_sun', 'cla_vis', 'cla_clht', 'cla_clamt', 'date2']
 
 weatherHead = pd.read_csv('hly1175.csv',skiprows = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15))
 weatherHead['date2'] = [dt.datetime.strptime(d,"%d-%b-%Y %H:%M") for d in weatherHead['date'] ]
 weatherHead['date_for_merge'] = weatherHead['date2'].dt.round('H')
-weatherHead = weatherHead[(weatherHead['date2'] >= '2021-04-01') & (weatherHead['date2'] < '2022-04-01')]
+weatherHead = weatherHead[(weatherHead['date2'] >= '2019-04-01') & (weatherHead['date2'] < '2022-04-01')]
 may_weatherHead =weatherHead.drop(['date_for_merge','date'], axis=1)
 may_weatherHead.columns = ['may_ind', 'may_rain', 'may_ind.1', 'may_temp', 'may_ind.2', 'may_wetb', 'may_dewpt', 'may_vappr','may_rhum', 'may_msl', 'may_ind.3', 'may_wdsp', 'may_ind.4', 'may_wddir', 'date2']
 
 weatherHead = pd.read_csv('hly2075.csv',skiprows = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15))
 weatherHead['date2'] = [dt.datetime.strptime(d,"%d-%b-%Y %H:%M") for d in weatherHead['date'] ]
 weatherHead['date_for_merge'] = weatherHead['date2'].dt.round('H')
-weatherHead = weatherHead[(weatherHead['date2'] >= '2021-04-01') & (weatherHead['date2'] < '2022-04-01')]
+weatherHead = weatherHead[(weatherHead['date2'] >= '2019-04-01') & (weatherHead['date2'] < '2022-04-01')]
 sli_weatherHead =weatherHead.drop(['date_for_merge','date'], axis=1)
 sli_weatherHead.columns = ['sli_ind', 'sli_rain', 'sli_ind.1', 'sli_temp', 'sli_ind.2', 'sli_wetb', 'sli_dewpt', 'sli_vappr','sli_rhum', 'sli_msl', 'sli_ind.3', 'sli_wdsp', 'sli_ind.4', 'sli_wddir', 'date2']
 
 weatherHead = pd.read_csv('hly775.csv',skiprows = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15))
 weatherHead['date2'] = [dt.datetime.strptime(d,"%d-%b-%Y %H:%M") for d in weatherHead['date'] ]
 weatherHead['date_for_merge'] = weatherHead['date2'].dt.round('H')
-weatherHead = weatherHead[(weatherHead['date2'] >= '2021-04-01') & (weatherHead['date2'] < '2022-04-01')]
+weatherHead = weatherHead[(weatherHead['date2'] >= '2019-04-01') & (weatherHead['date2'] < '2022-04-01')]
 cor_weatherHead =weatherHead.drop(['date_for_merge','date'], axis=1)
 cor_weatherHead.columns = ['cor_ind', 'cor_rain', 'cor_ind.1', 'cor_temp', 'cor_ind.2', 'cor_wetb', 'cor_dewpt', 'cor_vappr','cor_rhum', 'cor_msl', 'cor_ind.3', 'cor_wdsp', 'cor_ind.4', 'cor_wddir', 'date2']
 
@@ -48,16 +48,21 @@ cor_weatherHead.columns = ['cor_ind', 'cor_rain', 'cor_ind.1', 'cor_temp', 'cor_
 weatherHead = pd.read_csv('hly1475.csv',skiprows = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15))
 weatherHead['date2'] = [dt.datetime.strptime(d,"%d-%b-%Y %H:%M") for d in weatherHead['date'] ]
 weatherHead['date_for_merge'] = weatherHead['date2'].dt.round('H')
-weatherHead = weatherHead[(weatherHead['date2'] >= '2021-04-01') & (weatherHead['date2'] < '2022-04-01')]
+weatherHead = weatherHead[(weatherHead['date2'] >= '2019-04-01') & (weatherHead['date2'] < '2022-04-01')]
 tip_weatherHead =weatherHead.drop(['date_for_merge','date'], axis=1)
 tip_weatherHead.columns = ['tip_ind', 'tip_rain', 'tip_ind.1', 'tip_temp', 'tip_ind.2', 'tip_wetb', 'tip_dewpt', 'tip_vappr','tip_rhum', 'tip_msl', 'tip_ind.3', 'tip_wdsp', 'tip_ind.4', 'tip_wddir', 'date2']
 
 weatherHead = pd.read_csv('hly1575.csv',skiprows = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21))
 weatherHead['date2'] = [dt.datetime.strptime(d,"%d-%b-%Y %H:%M") for d in weatherHead['date'] ]
 weatherHead['date_for_merge'] = weatherHead['date2'].dt.round('H')
-weatherHead = weatherHead[(weatherHead['date2'] >= '2021-04-01') & (weatherHead['date2'] < '2022-04-01')]
+weatherHead = weatherHead[(weatherHead['date2'] >= '2019-04-01') & (weatherHead['date2'] < '2022-04-01')]
 don_weatherHead =weatherHead.drop(['date_for_merge','date'], axis=1)
-don_weatherHead.columns = ['ind', 'don_rain', 'don_ind.1', 'don_temp', 'don_ind.2', 'don_wetb', 'don_dewpt', 'don_vappr', 'don_rhum', 'don_msl', 'don_ind.3', 'don_wdsp', 'don_ind.4', 'don_wddir', 'don_ww', 'don_w', 'don_sun', 'don_vis', 'don_clht', 'don_clamt', 'don_date2']
+don_weatherHead.columns = ['ind', 'don_rain', 'don_ind.1', 'don_temp', 'don_ind.2', 'don_wetb', 'don_dewpt', 'don_vappr', 'don_rhum', 'don_msl', 'don_ind.3', 'don_wdsp', 'don_ind.4', 'don_wddir', 'don_ww', 'don_w', 'don_sun', 'don_vis', 'don_clht', 'don_clamt', 'date2']
+
+
+
+weatherHead = pd.merge(ker_weatherHead, pd.merge(dub_weatherHead, pd.merge(cla_weatherHead, pd.merge(may_weatherHead,pd.merge(sli_weatherHead, pd.merge(cor_weatherHead, pd.merge(don_weatherHead,tip_weatherHead, on = 'date2'),on = 'date2'), on = 'date2'),on = 'date2'), on = 'date2'), on = 'date2'), on = 'date2')
+
 
 
 
